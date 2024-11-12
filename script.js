00ac5a799eb61f16f95ae027935050496da3a0f5
MalkoAl <alexeymalko@gmail.com>
2024-11-12 17:39:20 +0200

    some in file script

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let obj = {
     "age":1850
     };
 console.log(obj);
+//alert(obj)
 
 for (key in obj) {
     //console.log(key);
     console.log(obj[key]);
 }
+//const itsConfirm = confirm("Please confirm ")
+//console.log(itsConfirm)
