8fb8fd011406b413c377e9ad3d8c3f25553c29d3
MalkoAl <alexeymalko@gmail.com>
2024-11-12 16:36:48 +0200

    add file script

---
+++ script.js
@@ -0,0 +1,14 @@
+let a=10;
+console.log(a);
+
+console.log(typeof 3);
+let obj = {
+    "city": "Kyiv",
+    "age":1850
+    };
+console.log(obj);
+
+for (key in obj) {
+    //console.log(key);
+    console.log(obj[key]);
+}
